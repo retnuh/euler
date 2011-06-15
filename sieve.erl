3e918f52f8c8f430a7b3508aefbc8683696d7204
Hunter Kelly <retnuh@gmail.com>
2011-06-15 01:17:59 +0100

    On-demand, growable version of Euler's version of the sieve of Erotsathenes.  (Or is it? Whatevs).

--- sieve.erl
+++ sieve.erl
@@ -22,9 +22,13 @@
 %%% -------------------------------------------------------------------
 -module(sieve).
 
--export([start/0, init/0, stop/0, primes_upto/1, first_n/1, next_prime/1, nth_prime/1]).
+%% TODO this is bad
+-compile(export_all).
+%-export([start/0, init/0, stop/0, primes_upto/1, first_n/1, first_prime/0, next_prime/1, nth_prime/1]).
 
 -include_lib("eunit/include/eunit.hrl").
+
+-define(CHUNK, 65536).  % Size to "grow" by
 
 start() ->
     Started = lists:member(?MODULE, registered()),
@@ -60,13 +64,20 @@ first_n(N) ->
             Primes
     end.
 
-%% Return the next Prime greater than N.  N need not be a prime itself
-%% (but it is faster if it is).
-next_prime(N) ->
-    ?MODULE ! {self(), next, N},
+%% Return the first prime and a token to iterate subsequent primes.
+first_prime() ->
+    ?MODULE ! {self(), first},
     receive
-        {ok, Prime} ->
-            Prime
+        {ok, Prime, Token} ->
+            {Prime, Token}
+    end.
+
+%% Return the next Prime in the sequence represented by Token
+next_prime(Token) ->
+    ?MODULE ! {self(), next, Token},
+    receive
+        {ok, Prime, T} ->
+            {Prime, T}
     end.
 
 %% Return the nth Prime.
@@ -78,87 +89,112 @@ nth_prime(N) ->
     end.
 
 init() ->
-    Dict = dict:store(2, 3, dict:new()),
-    Queue = queue:from_list([2,3]),
-    loop(Dict, Queue, 3, [make_filter(3)]).
+    {Set, Highest} = ensure_highest(gb_sets:singleton(2), 2, 100),
+    loop(Set, Highest).
 
-loop(Dict, Queue, HighestPrime, Filters) ->
+loop(Set, Highest) ->
     receive
         {Pid, stop} ->
             Pid ! unregister(?MODULE),
             ?debugFmt("Stop (server) - registered: ~p", [?debugVal(lists:member(sieve, registered()))]);
         {Pid, upto, Max} ->
-            {D, Q, H, F} = find(Dict, Queue, HighestPrime, Filters, Max),
-            Primes = queue:filter(fun(P) -> P =< Max end, Q),
-            Pid ! {ok, queue:to_list(Primes)},
-            loop(D, Q, H, F);
+            {S, H} = ensure_highest(Set, Highest, Max),
+            Primes = gb_sets:filter(fun(P) -> P =< Max end, S),
+            Pid ! {ok, gb_sets:to_list(Primes)},
+            loop(S, H);
         {Pid, first_n, N} ->
-            {D, Q, H, F} = ensure_n(Dict, Queue, HighestPrime, Filters, N),
-            {Primes, _} = queue:split(N, Q),
-            Pid ! {ok, queue:to_list(Primes)},
-            loop(D, Q, H, F);
+            {S, H} = ensure_n_primes(Set, Highest, N),
+            Pid ! {ok, take(N, S)},
+            loop(S, H);
         {Pid, nth, N} ->
-            {D, Q, H, F} = ensure_n(Dict, Queue, HighestPrime, Filters, N),
-            {Primes, _} = queue:split(N, Q),
-            Pid ! {ok, queue:get_r(Primes)},
-            loop(D, Q, H, F);
-        {Pid, next, N} ->
-            {D, Q, H, F} = find(Dict, Queue, HighestPrime, Filters, N + 2),
-            Val = case dict:find(N, D) of
-                      {ok, V} ->
-                          V;
-                      error ->
-                          queue:get(queue:filter(fun(P) -> P > N end, Q))
-                  end,
+            {S, H} = ensure_n_primes(Set, Highest, N),
+            {Val, _} = iter(N, gb_sets:iterator(S)),
             Pid ! {ok, Val},
-            loop(D, Q, H, F)
+            loop(S, H);
+        {Pid, first} ->
+            {First, Iter} = gb_sets:next(gb_sets:iterator(Set)),
+            Pid ! {ok, First, {2, gb_sets:size(Set), Iter}},
+            loop(Set, Highest);
+        {Pid, next, Token} ->
+            {N, Size, I1} = Token,
+            {S1, H1} = case (Size - N) of
+                           X when X >= 0 ->
+                               {Val, I2} = gb_sets:next(I1),
+                               Pid ! {ok, Val, {N+1, Size, I2}},
+                               {Set, Highest};
+                           _ ->
+                               %% Iterator doesn't go far enough
+                               {S, H} = ensure_n_primes(Set, Highest, N),
+                               {Val, Iter} = iter(N, gb_sets:iterator(S)),
+                               Pid ! {ok, Val, {N+1, gb_sets:size(S), Iter}},
+                               {S, H}
+                       end,
+            loop(S1, H1)
     end.
+
+take(N, S) ->
+    lists:reverse(take(N, gb_sets:iterator(S), [])).
+take(0, _, List) ->
+    List;
+take(N, Iter, List) ->
+    {Val, I2} = gb_sets:next(Iter),
+    take(N-1, I2, [Val | List]).
+
+iter(1, I1) ->
+    gb_sets:next(I1);
+iter(N, I1) ->
+    {_, I2} = gb_sets:next(I1),
+    iter(N - 1, I2).
 
 % Make sure that there are at least N primes
-ensure_n(Dict, Queue, HighestPrime, Filters, N) when length(Filters) >= N-1 ->
-    {Dict, Queue, HighestPrime, Filters};
-ensure_n(Dict, Queue, HighestPrime, Filters, N) ->
-    {D, Q, H, F} = find(Dict, Queue, HighestPrime, Filters, HighestPrime + 2),
-    ensure_n(D, Q, H, F, N).
-
-% Make sure all the primes upto Max have been found.
-find(Dict, Queue, HighestPrime, Filters, Max) when Max =< HighestPrime ->
-    {Dict, Queue, HighestPrime, Filters};
-find(Dict, Queue, HighestPrime, Filters, Max) ->
-    Next = find_next(HighestPrime, Filters),
-%    ?debugFmt("Finding next prime: ~p ~p ~p", [HighestPrime, Next, Max]),
-    find(dict:store(HighestPrime,Next, Dict), queue:in(Next, Queue), Next,
-         [make_filter(Next) | Filters], Max).
-
-make_filter(N) ->
-%    ?debugFmt("Making filter for ~p", [N]),
-    fun(X) -> X rem N /= 0 end.
-
-find_next(Start, Filters) ->
-    Next = Start + 2,
-    case lists:all(fun(F) -> F(Next) end, Filters) of
-        true ->
-            Next;
+ensure_n_primes(Set, Highest, N) ->
+    case gb_sets:size(Set) - N of
+        X when X >= 0 ->
+            {Set, Highest};
         _ ->
-            find_next(Next, Filters)
+            {S, H} = ensure_highest(Set, Highest, Highest + ?CHUNK),
+            ensure_n_primes(S, H, N)
     end.
-    
+
+% Make sure all the primes upto Desired have been found.
+ensure_highest(Set, Current, Desired) when Desired =< Current ->
+    {Set, Current};
+ensure_highest(Set, Current, Desired) ->
+    Start = case Current rem 2 of
+                0 -> Current + 1;
+                1 -> Current + 2
+            end,
+    % Skip the first one (2) since we don't generate even numbers
+    Candidates = apply_existing_sieve(gb_sets:next(element(2, gb_sets:next(gb_sets:iterator(Set)))),
+                                      lists:seq(Start, Desired, 2)),
+    S = sieve_candidates(Set, Candidates, euler_util:sqrt_int(Desired)),
+    {S, Desired}.
+
+apply_existing_sieve(none, List) ->
+    List;
+apply_existing_sieve({Prime, Iter}, List) ->
+    apply_existing_sieve(gb_sets:next(Iter), lists:filter(fun(X) -> X rem Prime /= 0 end, List)).
+
+sieve_candidates(Set, [Prime | Rest], Stop) when Prime > Stop ->
+    gb_sets:union(gb_sets:add(Prime, Set), gb_sets:from_list(Rest));
+sieve_candidates(Set, [], _) ->
+    Set;
+sieve_candidates(Set, [Prime | Rest], Stop) ->
+    sieve_candidates(gb_sets:add(Prime, Set), lists:filter(fun(X) -> X rem Prime /= 0 end, Rest), Stop).
+
+
 primes_test_() ->
-    {setup, fun() -> start() end, fun(_) -> stop() end,
+    {setup, fun start/0, fun(_) -> stop() end,
      [
       ?_assertEqual([2, 3, 5, 7, 11, 13, 17, 19, 23, 29], primes_upto(30)),
       ?_assertEqual([2, 3, 5, 7, 11, 13, 17, 19], primes_upto(20)),
       ?_assertEqual([2, 3], first_n(2)),
       ?_assertEqual([2], first_n(1)),
-      ?_assertEqual([2, 3, 5, 7, 11], first_n(5)),
-      ?_assertEqual(3, next_prime(2)),
-      ?_assertEqual(2, next_prime(-1)),
-      ?_assertEqual(23, next_prime(19)),
-      ?_assertEqual(23, next_prime(20))
+      ?_assertEqual([2, 3, 5, 7, 11], first_n(5))
      ]}.
 
 nth_primes_test_() ->
-    {setup, fun() -> start() end, fun(_) -> stop() end,
+    {setup, fun start/0, fun(_) -> stop() end,
      [
       ?_assertEqual(2, nth_prime(1)),
       ?_assertEqual(3, nth_prime(2)),
@@ -167,10 +203,17 @@ nth_primes_test_() ->
      ]}.
 
 next_primes_test_() ->
-    {setup, fun() -> start() end, fun(_) -> stop() end,
-     [
-      ?_assertEqual(3, next_prime(2)),
-      ?_assertEqual(5, next_prime(3)),
-      ?_assertEqual(23, next_prime(19)),
-      ?_assertEqual(29, next_prime(25))
-     ]}.
+    {setup, fun start/0, fun(_) -> stop() end,
+     {generator, ?MODULE, next_primes_test_generator}
+    }.
+
+next_primes_test_generator() ->
+    next_primes_test_generator(first_prime(), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]).
+next_primes_test_generator(_, []) ->
+    [];
+next_primes_test_generator({P, T}, [E | Rest]) ->
+    {generator, fun() ->
+                        [?_assertEqual(E, P),
+                         next_primes_test_generator(next_prime(T), Rest)]
+                end
+    }.
