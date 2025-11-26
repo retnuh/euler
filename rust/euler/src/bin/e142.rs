b9c190dbda19bb333ff83908f18d1cdf8d96d94a
Hunter Kelly <hkelly@riotgames.com>
2025-11-26 13:10:14 +0000

    WIP e142 e884

---
+++ rust/euler/src/bin/e142.rs
@@ -0,0 +1,86 @@
+#![feature(test)]
+extern crate test;
+
+use euler::util::timeit;
+use itertools::Itertools;
+// use num::ToPrimitive;
+
+fn is_perfect_square(n: u64) -> bool {
+    if n < 2 {
+        return true;
+    }
+    let sqrt_n = (n as f64).sqrt() as u64;
+    sqrt_n * sqrt_n == n
+}
+
+// Some algebra on the equations gives us this, where a-f are squares:
+// 2x = a + b
+// 2x = c + d
+// 2z = c - d
+// 2z = e - f
+// a + b = c + d
+// c - d = e - f
+// a + b = d + e - f
+fn constraints(a: u64, b: u64, c: u64, d: u64, e: u64, f: u64) -> bool {
+    a + b == c + d && c - d == e - f && a + b == d + e - f
+}
+
+fn e142() -> (u64, u64, u64) {
+    let mut x = 6;
+    let mut squares: Vec<u64> = vec![1, 4, 9, 16, 25];
+    loop {
+        let a = x * x;
+        let res = squares
+            .iter()
+            .permutations(5)
+            .find(|v| constraints(a, *v[0], *v[1], *v[2], *v[3], *v[4]));
+        match res {
+            Some(v) => {
+                let x = (a + v[0]) / 2;
+                let z = (v[3] - v[4]) / 2;
+                let y = a - x;
+                return (x, y, z);
+            }
+            None => {}
+        }
+        squares.push(x);
+        x += 1;
+    }
+}
+
+// fn e142_brute() -> u64 {
+//     let mut x: u64 = 2;
+//     'outer: loop {
+//         x += 1;
+//         for y in 2..x {
+//             for z in 1..y {
+//                 if !is_perfect_square(x + y) {
+//                     continue 'outer;
+//                 }
+//                 if !is_perfect_square(x - y) {
+//                     continue 'outer;
+//                 }
+//                 if !is_perfect_square(x + z) {
+//                     continue 'outer;
+//                 }
+//                 if !is_perfect_square(x - z) {
+//                     continue 'outer;
+//                 }
+//                 if !is_perfect_square(y + z) {
+//                     continue 'outer;
+//                 }
+//                 if !is_perfect_square(y - z) {
+//                     continue 'outer;
+//                 }
+//                 return x + y + z;
+//             }
+//         }
+//     }
+// }
+
+fn main() {
+    let ((x, y, z), seconds) = timeit(|| e142());
+    println!("// ans = {}", x + y + z);
+    println!("// x: {}\t, y: {}\tz: {}", x, y, z);
+    println!("// seconds:\t{}", seconds)
+}
---
+++ rust/euler/src/bin/e884.rs
@@ -0,0 +1,93 @@
+#![feature(test)]
+#![feature(iter_collect_into)]
+
+extern crate test;
+
+// use memoize::memoize;
+// use std::cmp::min;
+
+// use cached::proc_macro::cached;
+use num::integer::cbrt;
+use num::{pow, Integer};
+
+use euler::util::timeit;
+
+fn cubes(largest: u64) -> Vec<u64> {
+    let mut numbers: Vec<u64> = Vec::with_capacity(largest as usize);
+    for i in 1..=largest {
+        numbers.push(i * i * i)
+    }
+    numbers
+}
+
+fn s(upper: u64, upto: usize, cubes: &Vec<u64>) -> u64 {
+    (1..upper).fold(0, |acc, x| acc + d(x, cubes, upto))
+}
+
+fn e884(upper: u64) -> u64 {
+    let highest_cube_root = cbrt(upper - 1);
+    let cubes = cubes(highest_cube_root);
+    let highest_cube = *&cubes[(highest_cube_root - 1) as usize];
+    let repeated_sum = s(highest_cube + 1, highest_cube_root as usize, &cubes);
+    let (div, rem) = (upper - 1).div_rem(&highest_cube);
+    let partial_sum = s(rem + 1, highest_cube_root as usize, &cubes);
+    let repeat_count = div * (div + 1) / 2;
+    // don't over count the actual cubes!  hmm do need to account for final prime subtraction on remainder though
+    let repeats = repeated_sum * repeat_count;
+    let remainder_highest_cube_subtractions = div * rem;
+    let cube_adjustment = repeat_count * cubes.len() as u64;
+    repeats + remainder_highest_cube_subtractions + partial_sum - cube_adjustment
+}
+
+fn d(initial_n: u64, cubes: &Vec<u64>, largest: usize) -> u64 {
+    assert!(initial_n > 0);
+    if initial_n <= 7 {
+        return initial_n;
+    }
+    // only do this if initial_n > cubes[largest] ?
+    let (div, rem) = initial_n.div_rem(&cubes[largest - 1]);
+    let mut steps = div;
+    let n = rem;
+    if n > 0 {
+        steps += 1;
+        match &cubes[0..largest].binary_search(&n) {
+            Ok(_) => {}
+            Err(index) => {
+                assert!(*index > 0);
+                let nearest_cube = &cubes[*index - 1];
+                steps += d(rem - *nearest_cube, &cubes, *index + 1)
+            }
+        }
+    }
+    // println!("d({}):\t{}", initial_n, steps);
+    steps
+}
+
+// T(1000000000000):		128088830547982
+// seconds:	2.272181
+fn main() {
+    let n = pow(10, 17);
+    let (result, seconds) = timeit(|| e884(n));
+    println!("// S({}):\t{}", n, result);
+    println!("// seconds:\t{}", seconds)
+}
+
+#[test]
+fn test_e884() {
+    let cubes = cubes(4);
+    // assert_eq!(4, d(100, &cubes, 4));
+    // assert_eq!(5, d(101, &cubes, 4));
+    // assert_eq!(6, d(109, &cubes, 4));
+    // assert_eq!(8, d(34, &cubes, 4));
+    // assert_eq!(1, d(8, &cubes, 4));
+    // assert_eq!(2, d(35, &cubes, 4));
+    // assert_eq!(1, d(27, &cubes, 4));
+    // assert_eq!(1, d(64, &cubes, 4));
+    // assert_eq!(512, s(100, 4, &cubes));
+    let s_val = s(26, 4, &cubes);
+    println!("s(26):\t{}", s_val);
+    let e_val = e884(26);
+    println!("e884(26):\t{}", e_val);
+    assert_eq!(512, e884(100));
+    assert_eq!(s_val, e_val);
+}
