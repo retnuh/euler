fa2b5fc3371691b9a560a8898a209e311d5c67ab
Hunter Kelly <hkelly@riotgames.com>
2025-12-23 08:50:27 +0000

    Tweak e684 benchmark to not run brute-force (too slow); add just target for benchmarks

--- rust/euler/benches/e684.rs
+++ rust/euler/benches/e684.rs
@@ -3,7 +3,7 @@
 
 extern crate test;
 
-use euler::problems::e684::{little_s, little_s_brute, little_s_hk};
+use euler::problems::e684::{little_s, little_s_hk};
 use test::Bencher;
 
 #[bench]
@@ -14,7 +14,7 @@ fn e684_little_s_10000(b: &mut Bencher) {
 fn e684_little_s_hk_10000(b: &mut Bencher) {
     b.iter(|| little_s_hk(10000))
 }
-#[bench]
-fn e684_little_s_brute_10000(b: &mut Bencher) {
-    b.iter(|| little_s_brute(10000))
-}
+// #[bench]
+// fn e684_little_s_brute_10000(b: &mut Bencher) {
+//     b.iter(|| little_s_brute(10000))
+// }
